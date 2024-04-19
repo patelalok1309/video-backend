d59dc914a82c0bc073dd5650cb61597c7a8ef07d
patelalok1309 <alokdholu10@gmail.com>
2024-04-19 17:00:53 +0530

    added classes for api response and errors

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './src/.env' });
+
+import connectDB from "./db/index.js";
+import { app } from './app.js'
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
+
+
+
+
+
+
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHanlder = (requestHandler) => {
+    (req, res, next) => {
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .catch((err) => next(err))
+    }
+}
+
+export { asyncHanlder }
