8245dacb42185deeda4c9a3a6449a3b6dfc4a1bc
patelalok1309 <alokdholu10@gmail.com>
2024-04-22 16:22:09 +0530

    added login logout and auth middleware , testing is not done yet

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,9 +5,9 @@ import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
 // Handle user registration 
-const registerUser = asyncHanlder(async (req, res) =>{
+const registerUser = asyncHanlder(async (req, res) => {
 
-    const {fullName, email, username, password } = req.body
+    const { fullName, email, username, password } = req.body
 
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -30,7 +30,7 @@ const registerUser = asyncHanlder(async (req, res) =>{
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
-    
+
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -47,7 +47,7 @@ const registerUser = asyncHanlder(async (req, res) =>{
         fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
-        email, 
+        email,
         password,
         username: username.toLowerCase()
     })
@@ -68,11 +68,105 @@ const registerUser = asyncHanlder(async (req, res) =>{
 
 
 // Handle user login, access token and refresh tokens 
-const loginUser = asyncHanlder(async (req ,res) => {
+const loginUser = asyncHanlder(async (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    if (!(username || email)) {
+        throw new ApiError(400, "username or email is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    });
+
+    if (!user) {
+        throw new ApiError(404, 'User does not exist');
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, 'Invalid user credentials');
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select(" -password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser,
+                    refreshToken,
+                    accessToken,
+                    msg: "User Logged in successfully"
+                },
+            )
+        )
+
+})
+
+const logOutUser = asyncHanlder(async (req, res) => {
+
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined,
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logout Successfully"));
 
 })
 
 
+// generate access and refresh tokens 
+const generateAccessAndRefreshTokens = async (userId) => {
 
-export { registerUser , loginUser };
+    try {
+        const user = await User.find(userId);
+
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+
+
+    } catch (error) {
+        throw new ApiError(500, 'Something went wrong while generating access and refresh tokens')
+    }
+
+}
+
+
+
+export { registerUser, loginUser, logOutUser };
 
