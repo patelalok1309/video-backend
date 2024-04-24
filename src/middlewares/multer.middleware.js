34f17c9e854080cc06f9a3d19022ac1ee616860b
patelalok1309 <alokdholu10@gmail.com>
2024-04-24 10:25:50 +0530

    deployment 3

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,9 @@
 import multer from 'multer'
+import path from 'path'
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "./public/temp")
+        cb(null, path.join(__dirname , 'public', 'temp'));
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + file.originalname)
