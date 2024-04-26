01876e00dc51bcfa4975b251617c4162bd89971a
patelalok1309 <alokdholu10@gmail.com>
2024-04-26 16:05:05 +0530

    added playlist controller , models and routes tested

--- src/app.js
+++ src/app.js
@@ -23,9 +23,13 @@ app.get('/check-health', (req ,res) => {
 import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
+
+
 // Routes declarations 
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/videos', videoRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
+app.use('/api/v1/playlists',playlistRouter)
 
 export { app }
